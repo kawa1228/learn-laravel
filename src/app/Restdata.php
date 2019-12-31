eae3188d5f2cd96d5004d88e3dd9e442e476dbcf
kawa1228 <sxa.1105bo@gmail.com>
2019-12-31 13:10:31 +0900

    Restdataモデルを修正

--- src/app/Restdata.php
+++ src/app/Restdata.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Restdata extends Model
 {
-    //
+    protected $table = 'restdata';
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'message' => 'required',
+        'url' => 'required'
+    );
+
+    public function getData()
+    {
+        return $this->id . ':' . $this->message
+            . '(' . $this->url . ')';
+    }
 }
