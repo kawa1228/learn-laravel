0bed49f20c72fd69f3b2018b0aa5e7bf0f4aba77
kawa1228 <sxa.1105bo@gmail.com>
2020-01-01 10:43:26 +0900

    セッションの保存先をデータベースにする

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -17,13 +17,11 @@ class HelloController extends Controller
 
     public function ses_get (Request $request) {
         $sesdata = $request->session()->get('msg');
-        dump($sesdata);
         return view('hello.session', ['session_data' => $sesdata]);
     }
 
     public function ses_put (Request $request) {
         $msg = $request->input;
-        dump($msg);
         $request->session()->put('msg', $msg);
         return redirect('hello/session');
     }
