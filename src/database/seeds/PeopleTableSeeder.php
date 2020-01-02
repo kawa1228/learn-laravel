d65463548c1e51a52adb0ab828734e4ff8fd189a
kawa1228 <sxa.1105bo@gmail.com>
2020-01-02 12:36:46 +0900

    peopleテーブルのシーダーファイルを作成

---
+++ src/database/seeds/PeopleTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // 追記
+
+class PeopleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'name' => 'taro',
+            'mail' => 'taro@yamada.jp',
+            'age' => 12,
+        ];
+        DB::table('people')->insert($param);
+    }
+}
